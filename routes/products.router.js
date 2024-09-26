90f4d332acf079390a40d3f6740e5ed7b5980805
Alexander Ortiz Alonso <alexluthor68@gmail.com>
2024-09-26 13:33:50 -0500

    Clase 11 put, patch y delete. Utilizamos los metodos de patch y delete; para ello modificamos los archivos: routes/products.router.js y routes/users.router.js

--- routes/products.router.js
+++ routes/products.router.js
@@ -40,4 +40,22 @@ router.post('/', (req, res) => {
   });
 });
 
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: 'Update partial',
+    data: body,
+    id,
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: 'delete',
+    id,
+  });
+});
+
 module.exports = router;
--- routes/users.router.js
+++ routes/users.router.js
@@ -5,15 +5,17 @@ const { faker } = require('@faker-js/faker');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  const { limit, offset } = req.query;
-  if (limit && offset) {
-    res.json({
-      limit,
-      offset
-  });
-  } else {
-    res.send('No hay parámetros');
+  const users = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let i = 0; i < limit; i++) {
+    users.push({
+      name: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      image: faker.image.url(),
+    });
   }
+  res.json(users);
 });
 
 module.exports = router;
