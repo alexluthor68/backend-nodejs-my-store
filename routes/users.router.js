eefc0f5224c45145942ea8ab0c7a1046fe43cbde
Alexander Ortiz Alonso <alexluthor68@gmail.com>
2024-09-27 18:27:06 -0500

    Se modifica el archivo 'users.router.js, se le agrega nuevo codigo al endopoint y se agreda el codigo para aplicar los metodos: post, patch y delete

--- routes/users.router.js
+++ routes/users.router.js
@@ -18,4 +18,41 @@ router.get('/', (req, res) => {
   res.json(users);
 });
 
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  res.json({
+    id,
+    name: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    image: faker.image.imageUrl(),
+  });
+});
+
+router.post('/', (req, res) => {
+  const body = req.body;
+  res.json({
+    message: 'created',
+    data: body,
+  });
+});
+
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  res.json({
+    message: 'Update partial',
+    data: body,
+    id,
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: 'delete',
+    id,
+  });
+});
+
 module.exports = router;
