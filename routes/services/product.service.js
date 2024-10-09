ca19a3a04598103b4a50d1e6ec1f58273e960742
Alexander Ortiz Alonso <alexluthor68@gmail.com>
2024-10-08 19:26:21 -0500

    Clase 19 Manejo de errores con BOOM, se continua modificando el archivo 'product.service.js, se trabaja con el codigo: 'isBlock' para bloquear los productos que no queremos que vea el cliente

--- routes/services/product.service.js
+++ routes/services/product.service.js
@@ -15,7 +15,8 @@ class productsService {
         id: faker.string.uuid(),
         name: faker.commerce.productName(),
         price: parseInt(faker.commerce.price(), 10),
-        image: faker.image.url()
+        image: faker.image.url(),
+        isBlock: faker.datatype.boolean(),
       });
     }
   }
@@ -41,6 +42,9 @@ class productsService {
     const product = this.products.find(item => item.id === id);
     if (!product) {
       throw boom.notFound('Product not found');
+    }
+    if (product.isBlock) {
+      throw boom.conflict('Product is block');
     }
     return product;
   }
