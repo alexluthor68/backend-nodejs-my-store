07e85724a5fbee8f0680cb402136a304e3ed78a1
Alexander Ortiz Alonso <alexluthor68@gmail.com>
2024-10-10 14:29:58 -0500

    Clase 20 Validacion de datos. En el archivo de validator.handler.js, a la funcion validatorHandler se la transforma a una funcion que contenga el formato middleware dinamico, como tambien se importa la libreria boom

--- middlewares/validator.handler.js
+++ middlewares/validator.handler.js
@@ -1,9 +1,14 @@
-function validatorHandler(req, res, next) {
-  console.log(errorHandler);
-  res.status(500).json({
-    message: err.message,
-    stack: err.stack
-  });
+const boom = require('@hapi/boom');
+
+function validatorHandler(schema, property) {
+  return (req, res, next) => {
+    const data = req[property];
+    const { error } = schema.validate(data);
+    if (error) {
+      next(boom.badRequest(error));
+    }
+    next();
+  }
 }
 
 module.exports = validatorHandler;
