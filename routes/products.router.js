1fc096f6a798e62f78961cfa1f2d00777d1256ff
Alexander Ortiz Alonso <alexluthor68@gmail.com>
2024-10-10 19:42:28 -0500

    Clase 21 Probando los endpoints, en el products.router.js se importo el validatorHandler y los 3 esquemas; se trabajo el middleware dinamico en cada uno de los endpoints

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,6 +1,8 @@
 const express = require('express');
 
 const productsService = require('./services/product.service');
+const validatorHandler = require('./../middlewares/validator.handler');
+const { createProductSchema, updateProductSchema, getProductSchema } = require('./../schemas/product.schema');
 
 const router = express.Router();
 const service = new productsService();
@@ -14,7 +16,9 @@ router.get('/filter', (req, res) => {
   res.send('Este es un endpoint Selectivo de FILTER');
 });
 
-router.get('/:id', async (req, res, next) => {
+router.get('/:id',
+  validatorHandler(getProductSchema, 'params'),
+  async (req, res, next) => {
   try {
     const { id } = req.params;
     const products = await service.findOne(id);
@@ -22,15 +26,22 @@ router.get('/:id', async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-});
+  }
+);
 
-router.post('/', async (req, res) => {
+router.post('/',
+  validatorHandler(createProductSchema, 'body'),
+  async (req, res) => {
   const body = req.body;
   const newProduct = await service.create(body);
   res.status(201).json(newProduct);
-});
+  }
+);
 
-router.patch('/:id', async (req, res, next) => {
+router.patch('/:id',
+  validatorHandler(getProductSchema, 'params'),
+  validatorHandler(updateProductSchema, 'body'),
+  async (req, res, next) => {
   try {
     const { id } = req.params;
     const body = req.body;
@@ -39,7 +50,8 @@ router.patch('/:id', async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-});
+  }
+);
 
 router.delete('/:id', async (req, res) => {
   const { id } = req.params;
