8f90ab953c8f5e9a3829f6fb96cf35e9721890b0
Alexander Ortiz Alonso <alexluthor68@gmail.com>
2024-10-12 18:47:23 -0500

    Clase 24 Problema de CORS. Se crea un array whitelist que va a contener las aplicaciones controladas a las cuales se les va a dar acceso para que el cliente haga peticiones y se configura todo el codigo correspondiente para dar los accesos

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ const cors = require('cors');
 const routerApi = require('./routes');
 
 const { logErrors, errorHandler, boomErrorHandler } = require('./middlewares/error.handler');
+const { Callback } = require('puppeteer');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use(cors());
+
+const whitelist = ['http://localhost:8080', 'https://myapp.co'];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  }
+}
+app.use(cors(options));
 
 app.get('/', (req, res) => {
   res.send('Hola mi server en express');
